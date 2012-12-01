23454e14b4bfbcaba78032a9846a3195121e9a87
Blake Haswell <haswell00@gmail.com>
2012-12-01 22:31:38 +1100

    Added static asset support.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var app = express();
 
+// I like EJS. :-)
 app.set("view engine", "ejs");
+
+// Serve static assets.
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
     res.render("index");
